e57ebfa3f980b2d53e8d66ab65b8320feaa0be74
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-13 07:52:09 -1000

    countdown timer created

--- app/src/main/java/com/myappcompany/russell/timerdemo/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/timerdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.myappcompany.russell.timerdemo;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new CountDownTimer(10000,1000){
+          public void onTick(long milisecondsUntilDone){
+              Log.i("Seconds Left!", String.valueOf(milisecondsUntilDone/1000));
+          }
+
+          public void onFinish(){
+              Log.i("We're done!!!", "No more countdown");
+          }
+        }.start();
+
+
+//        final Handler handler = new Handler();
+//
+//        Runnable run = new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.i("Hey it's us", "A second has passed by");
+//
+//                handler.postDelayed(this,1000);
+//            }
+//        };
+//
+//        handler.post(run);
+
+
     }
 }
